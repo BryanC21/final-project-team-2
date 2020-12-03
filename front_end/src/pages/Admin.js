aa5f004ddc0b17cb60dd3f1dc8a204ee01809e66
Sweept <emersonsridhar@gmail.com>
2020-12-02 22:02:28 -0800

    More skeletons, commit#2

--- front_end/src/pages/Admin.js
+++ front_end/src/pages/Admin.js
@@ -1,1 +1,19 @@
 // todo
+import React from 'react';
+
+import ViewListings from '../components/ViewListings';
+import ListingCreationForm from '../components/ListingCreationForm';
+import Inquiries from '../components/Inquiries';
+
+const Admin = () => {
+    return (
+        <div>
+            <h1>Admin</h1>
+            <ListingCreationForm />
+            <Inquiries />
+            <ViewListings />
+        </div>
+    );
+};
+
+export default Admin;
--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -1,1 +1,12 @@
 // todo
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
