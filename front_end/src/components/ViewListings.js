4329a912d504e38c474d37d93e214a90892d37dc
Sweept <emersonsridhar@gmail.com>
2020-12-04 19:39:10 -0800

    commit #5, fixed ViewListing.js

--- front_end/src/components/ViewListings.js
+++ front_end/src/components/ViewListings.js
@@ -1,94 +1,41 @@
-// todo
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
-import { useDispatch } from 'react-redux';
-import { setInquiry } from '../redux/actions/inquiryActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { setListings } from '../redux/actions/listingActions';
+import Listing from './Listing';
 
-const Listing = (props) => {
+const ViewListings = (props) => {
 
     const dispatch = useDispatch();
-    const [stateInquiry, setStateInquiry] = React.useState('');
-    //const test = useSelector(state => state.inquiryReducer.inquiries);
+    const listings = useSelector(state => state.listingReducer.listings);
 
-    const handleDelete = () => {
-        axios.get(`/api/deleteListing?id=${props.listing.id}`);
-        console.log('delete was clicked');
-        /*axios.get('/api/viewListings')
-          .then(function (response) {
-            dispatch(setListings(response.data.items))
-            //console.log(response);
-          })
-          .catch(function (error) {
-            //console.log(error);
-          });*/
-    };
-    const handleView = () => {
-        axios.get(`/api/getInquiries?listingId=${props.listing.id}`)
+    useEffect(() => {
+        axios.get('/api/viewListings')
             .then(function (response) {
-                //console.log(response.data.inquiries)
-                dispatch(setInquiry(response.data.inquiries));
-                //console.log(test)
+                dispatch(setListings(response.data.items));
                 //console.log(response);
             })
             .catch(function (error) {
                 console.log(error);
             });
-    };
-    const handleSubmit = () => {
-        axios.post(`/api/makeInquiry?listingId=${props.listing.id}`, {
-            message: stateInquiry
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+    }, []);
+
+    const createListingsElements = (listing) => {
+        return (
+            <Listing
+                listing={listing}
+                userMode={props.userMode}
+            />
+        );
     };
 
     return (
-        <div key={Math.random()}>
-            <table className="listing">
-                <tbody>
-                    <tr>
-                        <td>Description:
-              <div>{props.listing.description}</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Type:
-              <div>{props.listing.type}</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Price:$
-              <div>{props.listing.price}</div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Title:
-              <div>{props.listing.title}</div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            {!props.userMode && (
-                <div>
-                    <button onClick={handleDelete}>Delete</button>
-                    <button onClick={handleView}>View Inquiries</button>
-                </div>
-            )}
-            {props.userMode && (
-                <div>
-                    <textarea id="inquiry" value={stateInquiry}
-                        onChange={(e) => setStateInquiry(e.target.value)}
-                    />
-                    <button className="submit" key={Math.random()} onClick={handleSubmit}>Submit</button>
-                </div>
-            )}
-        </div >
+        <div>
+            <h1>ViewListings</h1>
+            {listings.map(items => createListingsElements(items))}
+        </div>
     );
 };
 
-export default Listing;
+export default ViewListings;
